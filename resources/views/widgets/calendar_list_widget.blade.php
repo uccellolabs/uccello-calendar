14291714122c0d3ad15c366693755a34bbae9d34
Jonathan SARDO <sardoj@gmail.com>
2020-06-05 12:26:49 +0200

    fix(widget): Displays only if module is active

--- resources/views/widgets/calendar_list_widget.blade.php
+++ resources/views/widgets/calendar_list_widget.blade.php
@@ -4,32 +4,35 @@
     $datatableColumns = Uccello::getDatatableColumns($relatedModule, null, 'related-list');
     $datatableContentUrl = ucroute('uccello.list.content', $domain, $relatedModule, ['id' => $recordId, 'src_module' => $module->id])
 ?>
-<div class="card" style="margin: 0;">
-    <div class="card-content">
-        {{-- Title --}}
-        <span class="card-title">
-            {{-- Icon --}}
-            <i class="material-icons left primary-text">{{ $relatedModule->icon }}</i>
 
-            {{-- Label --}}
-            {{ uctrans($relatedModule->name, $relatedModule) }}
+@if ($relatedModule && $relatedModule->isActiveOnDomain($domain))
+    <div class="card" style="margin: 0;">
+        <div class="card-content">
+            {{-- Title --}}
+            <span class="card-title">
+                {{-- Icon --}}
+                <i class="material-icons left primary-text">{{ $relatedModule->icon }}</i>
 
-            <div class="right-align right">
-                <select id="calendar-period">
-                    <option value="all" selected>{{ trans('calendar::calendar.period.all') }}</option>
-                    <option value="today">{{ trans('calendar::calendar.period.today') }}</option>
-                    <option value="week">{{ trans('calendar::calendar.period.week') }}</option>
-                    <option value="month">{{ trans('calendar::calendar.period.month') }}</option>
-                    <option value="quarter">{{ trans('calendar::calendar.period.quarter') }}</option>
-                </select>
-            </div>
-        </span>
+                {{-- Label --}}
+                {{ uctrans($relatedModule->name, $relatedModule) }}
 
-        {{-- Table --}}
-        @include('uccello::modules.default.detail.relatedlists.table', [ 'datatableId' => $datatableId, 'datatableColumns' => $datatableColumns, 'relatedModule' => $relatedModule, 'datatableContentUrl' => $datatableContentUrl ])
+                <div class="right-align right">
+                    <select id="calendar-period">
+                        <option value="all" selected>{{ trans('calendar::calendar.period.all') }}</option>
+                        <option value="today">{{ trans('calendar::calendar.period.today') }}</option>
+                        <option value="week">{{ trans('calendar::calendar.period.week') }}</option>
+                        <option value="month">{{ trans('calendar::calendar.period.month') }}</option>
+                        <option value="quarter">{{ trans('calendar::calendar.period.quarter') }}</option>
+                    </select>
+                </div>
+            </span>
+
+            {{-- Table --}}
+            @include('uccello::modules.default.detail.relatedlists.table', [ 'datatableId' => $datatableId, 'datatableColumns' => $datatableColumns, 'relatedModule' => $relatedModule, 'datatableContentUrl' => $datatableContentUrl ])
+        </div>
     </div>
-</div>
 
-@section('script')
-{{ Html::script(mix('js/widgets/calendar_list.js', 'vendor/uccello/calendar')) }}
-@append
+    @section('script')
+    {{ Html::script(mix('js/widgets/calendar_list.js', 'vendor/uccello/calendar')) }}
+    @append
+@endif
