486e85b28ca571205ed74d2e066b725c08b22f50
Jonathan SARDO <sardoj@gmail.com>
2021-01-15 15:12:14 +0100

    fix(migration): Compatibility with Laravel > 5.7

--- database/migrations/2019_01_01_135714_create_calendar_accounts_table.php
+++ database/migrations/2019_01_01_135714_create_calendar_accounts_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateCalendarAccountsTable extends Migration
 {
@@ -15,7 +16,14 @@ class CreateCalendarAccountsTable extends Migration
     {
         Schema::create('calendar_accounts', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('user_id');
+
+            // Compatibility with Laravel < 5.8
+            if (DB::getSchemaBuilder()->getColumnType('users', 'id') === 'bigint') { // Laravel >= 5.8
+                $table->unsignedBigInteger('user_id');
+            } else { // Laravel < 5.8
+                $table->unsignedInteger('user_id');
+            }
+
             $table->string('service_name');
             $table->string('username');
             $table->text('token');
@@ -27,7 +35,7 @@ class CreateCalendarAccountsTable extends Migration
                 ->references('id')
                 ->on('users')
                 ->onDelete('cascade');
-            
+
             $table->timestamps();
         });
     }
--- database/migrations/2019_03_01_091556_create_config_table.php
+++ database/migrations/2019_03_01_091556_create_config_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateConfigTable extends Migration
 {
@@ -16,7 +17,14 @@ class CreateConfigTable extends Migration
         Schema::create('calendar_configs', function(Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('domain_id')->nullable();
-            $table->unsignedInteger('user_id')->nullable();
+
+            // Compatibility with Laravel < 5.8
+            if (DB::getSchemaBuilder()->getColumnType('users', 'id') === 'bigint') { // Laravel >= 5.8
+                $table->unsignedBigInteger('user_id')->nullable();
+            } else { // Laravel < 5.8
+                $table->unsignedInteger('user_id')->nullable();
+            }
+
             $table->text('data')->nullable();
             $table->timestamps();
 
